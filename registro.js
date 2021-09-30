c1b66f87fb62534aa38ac25411996fe13a83c580
Sergio Perez <sergio-luis98@hotmail.com>
2021-09-29 21:16:48 -0500

    agregacion de archivo registro y funcion agregarRegistro

---
+++ registro.js
@@ -0,0 +1,54 @@
+const arrayGlobal = [];
+
+const agregarRegistro = () => {
+  const nombre = document.getElementById('nombre').value;
+  const apellido = document.getElementById('apellido').value;
+  const telefono = document.getElementById('telefono').value;
+  const correo = document.getElementById('correo').value;
+  const contraseña = document.getElementById('contraseña').value;
+  const arrayForm = [];
+  const checkFunctionNombre = checkNombre(nombre);
+  const checkFunctionApellido = checkApellido(apellido);
+  const checkFunctionTelefono = checkTelefono(telefono);
+  const checkFunctionCorreo = checkCorreo(correo);
+  const checkFunctionContraseña = checkContrasena(contraseña);
+  if (
+    checkFunctionNombre &&
+    checkFunctionApellido &&
+    checkFunctionTelefono &&
+    checkFunctionCorreo &&
+    checkFunctionContraseña
+  ) {
+    arrayForm.push(nombre, apellido, telefono, correo, contraseña);
+    console.log(arrayForm);
+    arrayGlobal.push(arrayForm);
+  } else {
+    console.log('Error en el formulario');
+    if (!checkFunctionNombre) {
+      console.log('Error en el nombre');
+    }
+    if (!checkFunctionApellido) {
+      console.log('Error en el apellido');
+    }
+    if (!checkFunctionTelefono) {
+      console.log('Error en el telefono');
+    }
+    if (!checkFunctionCorreo) {
+      console.log('Error en el correo');
+    }
+    if (!checkFunctionContraseña) {
+      console.log('Error en la contraseña');
+    }
+  }
+  console.log(arrayGlobal);
+};
+
+const ordenarArreglo = (arreglo) => {};
+
+const filtrarCorreo = (arreglo) => {};
+
+module.exports = {
+  agregarRegistro,
+  ordenarArreglo,
+  filtrarCorreo,
+};
